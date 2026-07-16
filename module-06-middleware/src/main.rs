0f8c19f4cbcd28b06a834d16b8a2ca668b58d8d5
chuanan.liu <chuanan.liu@sistalk.com>
2026-07-16 10:05:22 +0800

    feat: 添加 Axum 日志中间件基础定义
    - 在 main.rs 中导入 axum/tower 相关中间件和 Trace 依赖
    - 实现 logging_middleware 基础结构以记录请求耗时和信息

--- module-06-middleware/src/main.rs
+++ module-06-middleware/src/main.rs
@@ -1,3 +1,30 @@
+// 中间件 与 Layer
+use axum::{
+    Router,
+    extract::Request,
+    http::{HeaderValue, Method, StatusCode, header},
+    middleware::{self, Next},
+    response::{IntoResponse, Response},
+    routing::get,
+};
+use std::time::{Duration, Instant};
+use tower::ServiceBuilder;
+use tower_http::{
+    compression::CompressionLayer,
+    cors::{Any, CorsLayer},
+    trace::TraceLayer,
+};
+use tracing::Level;
+
+async fn logging_middleware(request: Request, next: Next) -> Response {
+    let method = request.method().clone();
+    let uri = request.uri().clone();
+    let start = Instant::now();
+    let response = next.run(request).await;
+
+    response
+}
+
 fn main() {
     println!("Hello, world!");
 }
