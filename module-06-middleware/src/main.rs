809ac5df30df052447e9a22dba76da0eea4da7a4
chuanan.liu <chuanan.liu@sistalk.com>
2026-07-17 09:42:17 +0800

    feat: 添加日志记录和响应计时中间件
    - 在 logging_middleware 中使用 tracing::info! 记录请求方法、URI、状态码及耗时
    
    - 新增 timing_middleware 中间件，向响应中添加包含请求耗时的 X-Response-Time 响应头

--- module-06-middleware/src/main.rs
+++ module-06-middleware/src/main.rs
@@ -16,11 +16,31 @@ use tower_http::{
 };
 use tracing::Level;
 
+// 日志中间件， 记录每个请求的日志
 async fn logging_middleware(request: Request, next: Next) -> Response {
     let method = request.method().clone();
     let uri = request.uri().clone();
     let start = Instant::now();
     let response = next.run(request).await;
+    tracing::info!(
+        method = %method,
+        uri = %uri,
+        status = %response.status().as_u16(),
+        duration_ms = %start.elapsed().as_millis(),
+        "Request completed"
+    );
+    response
+}
+
+// 计时中间件， 添加X-Response-Time 响应头
+async fn timing_middleware(request: Request, next: Next) -> Response {
+    // Instant 单调时间，不会回退，不受系统时间调整影响，一直递增
+    let start = Instant::now();
+    let mut response = next.run(request).await;
+    response.headers_mut().insert(
+        "X-Response-Time",
+        HeaderValue::from_str(&format!("{}ms",start.elapsed().as_millis())).unwrap()
+    );
 
     response
 }
